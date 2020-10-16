87c514c8098f1039e235da853dec82d0158d2bad
phyokyawdev <phyokyawdev@gmail.com>
2020-10-16 22:00:45 +0630

    Added files and changes required for development to heroku

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 app.use(helmet());
 // set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = process.env.MONGO_URL ;
+var dev_db_url = process.env.MONGODB_URI_DEV;
+var mongoDB = process.env.MONGODB_URI || dev_db_url ;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));
